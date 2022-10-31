91fa263af3039c27c930049bc25cbf7b7bd1ce8c
salto122 <szysz@outlook.com>
2022-11-01 00:16:45 +0100

    adding edit and change password for admin

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -40,7 +40,108 @@ namespace IdentityAppCourse2022.Controllers
             }
 
             return View(userList);
+        }
 
+        public IActionResult Edit(string userId)
+        {
+            var user = _db.AppUser.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+            if (role != null)
+            {
+                user.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+
+            user.RoleList = _db.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(AppUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDbValue = _db.AppUser.FirstOrDefault(u => u.Id == user.Id);
+                if (userDbValue == null)
+                {
+                    return NotFound();
+                }
+
+                var userRole = _db.UserRoles.FirstOrDefault(u => u.UserId == userDbValue.Id);
+                if (userRole != null)
+                {
+                    var previousRoleName = _db.Roles.Where(u => u.Id == userRole.RoleId).Select(e => e.Name)
+                        .FirstOrDefault();
+                    await _userManager.RemoveFromRoleAsync(userDbValue, previousRoleName);
+                }
+
+                await _userManager.AddToRoleAsync(userDbValue, _db.Roles.FirstOrDefault(u => u.Id == user.RoleId).Name);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            user.RoleList = _db.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(user);
+        }
+
+        public IActionResult ChangePasswd(string userId) //TODO name refactor
+        {
+            var user = _db.AppUser.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+            if (role != null)
+            {
+                user.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+
+            user.RoleList = _db.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePasswd(AppUser changePasswdAdminViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(changePasswdAdminViewModel);
+            }
+
+            var user = await _userManager.FindByIdAsync(changePasswdAdminViewModel.Id);
+            if (user != null)
+            {
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                await _userManager.ResetPasswordAsync(user, token, changePasswdAdminViewModel.Password);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(changePasswdAdminViewModel);
         }
     }
 }
